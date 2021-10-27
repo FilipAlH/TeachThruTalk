05c5a30326de8438d926a025e6e693228983c771
Jeffrey.Beh <jeffrey.beh@hotmail.com>
2021-10-27 19:45:25 -0400

    db models

---
+++ models/index.js
@@ -0,0 +1,24 @@
+// import models
+const User = require('./User');
+const Language = require('./Language');
+const Thread = require('./Thread');
+const Reply = require('./Reply');
+
+Reply.belongsTo(Thread,{
+    foreignKey:'thread_id',
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+});
+
+Thread.hasMany(Reply,{
+    foreignKey:'thread_id',
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+});
+
+module.exports = {
+  User,
+  Language,
+  Thread,
+  Reply
+};
