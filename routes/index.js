7e3a09966846e29e2fb4187273b5e3de6ece856c
Aar Kaiser <dev@aarkaiser>
2021-10-28 12:00:41 -0400

    updating route/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,8 @@
-// Required
 const express = require("express");
 const languages = require("./languages");
 
-// Declaring App
 const app = express();
 
-// Middleware
 app.use("/language", languagesRouter);
 
-// Exporting app
 module.exports = app;
