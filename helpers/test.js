98e3cc0af3b94a2e6629d1a9ac4f79e7531cafbb
Aar Kaiser <dev@aarkaiser>
2021-10-30 08:43:42 -0400

    fixing branch

--- helpers/test.js
+++ helpers/test.js
@@ -4,3 +4,5 @@ const apiKey = process.env.API_KEY;
 const text = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur."
 
 translate(text, 'en', 'sp',apiKey)
+
+FIX BRANCH.
