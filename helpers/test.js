f413219d15cdddb502c877ee75e693dee487019a
Aar Kaiser <dev@aarkaiser>
2021-10-30 11:24:25 -0400

    updating translate with splits working properly

--- helpers/test.js
+++ helpers/test.js
@@ -1,8 +1,4 @@
 const { translate } = require("./translate");
-require('dotenv').config()
-const apiKey = process.env.API_KEY;
-const text = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur."
+const text = "Testing how to translate sentences"
 
-translate(text, 'en', 'sp',apiKey)
-
-FIX BRANCH.
+translate(text, 'en', 'sp')
