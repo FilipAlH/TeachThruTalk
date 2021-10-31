fcaa6d922ff3cf8c5c4a7e1bf9f3611fa48a8aa3
Aar Kaiser <dev@aarkaiser>
2021-10-31 16:34:35 -0400

    testing fetch

--- public/testtranslate.js
+++ public/testtranslate.js
@@ -5,11 +5,19 @@ $(document).ready(function () {
     let spn = '<span class="highlight">' + selectedText + "</span>";
     let text = $(".textcont").text();
     $(".textcont").html(text.replace(selectedText, spn));
-    console.log(selectedText);
-  });
-  $(document).on("mouseover", ".highlight", function () {
-    // alert("Works!");
-
-    console.log("Highlighted");
+    console.log(`"${selectedText}" was highlighted"`);
+    fetch("/api/translate/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      text: JSON.stringify(selectedText),
+      language: "de",
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   });
 });
