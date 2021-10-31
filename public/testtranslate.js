aaeb9477e35819b56c74ce0facd5dcf85f4baee4
Aar Kaiser <dev@aarkaiser>
2021-10-31 14:13:29 -0400

    integrating hover translate test

---
+++ public/testtranslate.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $(document).on("mouseup", ".textcont", function () {
+    let selectedText = window.getSelection().toString();
+    // console.log(selectedText);
+    let spn = '<span class="highlight">' + selectedText + "</span>";
+    let text = $(".textcont").text();
+    $(".textcont").html(text.replace(selectedText, spn));
+    console.log(selectedText);
+  });
+
+  // // Mouseover Function
+  // $(document).on("mouseover", ".highlight", function () {
+    
+  //   // alert("Works!");
+  //   console.log(response);
+  //   console.log("Highlighted");
+  // });
+});
---
+++ routes/api/translate.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { translateText } = require('../../helpers/translateText');
+
+
+
+
+module.exports = router;
