0257c095b6a09657ff71d4e48260b0e15df25e60
Aar Kaiser <dev@aarkaiser>
2021-10-30 20:27:21 -0400

    uploading test hover translator

---
+++ helpers/select_translate.js
@@ -0,0 +1,18 @@
+const {translateText}=require('../helpers/translateText');
+
+$(document).ready(function () {
+  $(document).on("mouseup", ".textcont", function () {
+    let selectedText = window.getSelection();
+    // console.log(selectedText);
+    let spn = '<span class="highlight">' + selectedText + "</span>";
+    let text = $(".textcont").text();
+    $(".textcont").html(text.replace(selectedText, spn));
+  });
+  $(document).on("mouseover", ".highlight", function () {
+    translateText(selectedText).then(response => response)
+    // alert("Works!");
+    console.log(response);
+    console.log("Highlighted");
+  });
+});
+
--- helpers/test.js
+++ helpers/test.js
@@ -1,6 +1,4 @@
 const { response } = require('express');
 const {translateText}=require('./translateText');
 
-
-
 translateText('Hola como estas?',"FR").then(response=>console.log(response));
