65d41d3055ba2a59cdf7d421ccb83ac31b9dde23
Jeffrey.Beh <jeffrey.beh@hotmail.com>
2021-11-01 08:30:22 -0400

    login route not working

--- routes/homepageRoute.js
+++ routes/homepageRoute.js
@@ -34,24 +34,28 @@ router.get('/login', async (req, res) => {
 });
 
 //post route for login info
-router.post('/loginRequest', async (req, res) => {
+router.post('/login', async (req, res) => {
     try {
-        const thisUser = req.body
-        const userReportedEmail = await User.findOne({ where: { email: req.body.email }})
-
+        const thisUser = req.body;
+        const userReportedEmail = await User.findOne({ where: { email: req.body.email }});
+        //const userReportedEmail=true;
         if(!userReportedEmail) {
-            res.status(400).json({message: "Incorrect email or password, please try again"})
+            res.status(400).json({message: "Incorrect email or password, please try again"});
+            return;
 
-        } else {
-            const validatePassword = bcrypt.compareSync(thisUser.password, userReportedEmail.dataValues.password)
+        }  
+            const validatePassword = bcrypt.compareSync(thisUser.password, userReportedEmail.dataValues.password);
+            //  const validatePassword =true;
             console.log(req.body)
             if(!validatePassword) {
                 res.status(400).json({message: "Incorrect email or password, please try again"})
-                return
-            }
-
-            res.json({ message: 'You are now logged in!' });
+                return;
         }
+        req.session.save(() => {
+            req.session.logged_in = true;
+            res.json.status(200).json('success');
+            
+          });
     } catch (error) {
         res.status(404).json(error);
     }
@@ -76,6 +80,12 @@ router.post('/signup', async (req, res) => {
             password: req.body.password,
             location: req.body.location
         })
+        
+        req.session.save(() => {
+            req.session.logged_in = true;
+      
+            res.status(200).json(userData);
+          });
         res.status(200).json(userData)
     } catch (error) {
         res.status(500).json(error)
@@ -94,4 +104,16 @@ router.get('/mapInfo', async(req, res) => {
         res.status(404).json(error)
     }
 })
+
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+
 module.exports = router
