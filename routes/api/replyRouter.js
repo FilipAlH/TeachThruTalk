a80e2ac7c8f3da6960f2b8f532532d687b28536b
Wickette <stephany.clark2@gmail.com>
2021-11-01 19:27:07 -0400

    style changes/updates

--- routes/api/replyRouter.js
+++ routes/api/replyRouter.js
@@ -1,19 +1,20 @@
 const router = require('express').Router();
-const { Reply } = require('../../models');
+const { Reply, Thread, User } = require('../../models');
 const withAuth =require('../../utils/auth');
 
 //Grab reply by thread id
 router.get('/:id', withAuth,async (req, res) => {
     try {
         const replyData = await Reply.findAll({
-            where: {
-                thread_id: req.params.id,
-            }
+            where: { thread_id: req.params.id },
+            include: [{ model: Thread }, { model: User }]
         });
-        console.log(replyData)
         const replies = replyData.map((reply) => reply.get({ plain: true }))
+        const thread = replyData[0].thread.title;
+        const user = replyData[0].user.name
+
         res.render('reply', {
-            replies , loggedIn: req.session.loggedIn
+            replies, thread, user, loggedIn: req.session.loggedIn
         })
     } catch (error) {
         res.status(500).json(error);
@@ -23,10 +24,12 @@ router.get('/:id', withAuth,async (req, res) => {
 //create a new reply
 router.post('/:id', async (req, res) => {
     // create a reply
+    console.log(req.params)
     try {
       const reply = await Reply.create({
         body: req.body.body,
-        thread_id: req.params.id
+        thread_id: req.params.id,
+        user_id: req.session.user_id
       });
       res.status(200).json(reply);
   
